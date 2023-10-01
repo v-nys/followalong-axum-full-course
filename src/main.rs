69ed3b0ebb50756f6a8cb7ef56a756530c0c0cbc
Vincent Nys <vincent.nys@ap.be>
2023-10-01 13:54:08 +0200

    Got query params working.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 use std::net::SocketAddr;
 
 use axum;
-use axum::response::Html;
+use axum::extract::Query;
+use axum::response::{Html, IntoResponse};
 use axum::routing;
+use serde::Deserialize;
 use tokio;
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    let routes = axum::Router::new().route(
-        "/hello",
-        routing::get(|| async { Html("<p>Hello, world!</p>") }),
-    );
+    let routes = axum::Router::new().route("/hello", routing::get(handler_hello));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8095));
     println!("->> LISTENING ON {addr}\n");
     axum::Server::bind(&addr)
         .serve(routes.into_make_service())
         .await
         .expect("Starting a server should work.");
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+    Html(format!(
+        "<p>Hello, {who}!</p>",
+        who = params.name.as_deref().unwrap_or("World!")
+    ))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -3,6 +3,6 @@ use anyhow::Result;
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let client = httpc_test::new_client("http://localhost:8095").expect("Failed to get client");
-    client.do_get("/hello").await?.print().await?;
+    client.do_get("/hello?name=vincent").await?.print().await?;
     Ok(())
 }
