6ffdbf5ed278b0417ae6d2ac0cabeae3c90679b8
Vincent Nys <vincent.nys@ap.be>
2023-10-03 18:58:48 +0200

    Think this would work for delete_ticket, but not ideal.

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ use axum::{http::StatusCode, response::IntoResponse};
 #[derive(Debug)]
 pub enum Error {
     LoginFail,
+    TicketDeleteFailIdNotFound { id: u64 }
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
 
-use crate::Result;
+use crate::{Result, Error};
 use serde::Serialize;
 
 #[derive(Serialize, Clone)]
@@ -27,23 +27,39 @@ impl ModelController {
 
     async fn create_ticket(&self, to_create: TicketForCreate) -> Result<Ticket> {
         // eventuele error is van ander type dan eigen error type...
-        let mut lock_result = self.tickets_store.lock().unwrap();
+        let mut guard = self.tickets_store.lock().unwrap();
         // mutexguard implementeert dereferencing, dus zou wel
-        let safe_id = lock_result.len() as u64;
+        let safe_id = guard.len() as u64;
         let ticket = Ticket {
             id: safe_id,
             title: to_create.title,
         };
         let clone = ticket.clone();
-        lock_result.push(Some(ticket));
+        guard.push(Some(ticket));
         Ok(clone)
     }
 
     async fn list_tickets(&self) -> Result<Vec<Ticket>> {
-        let lock_result = self.tickets_store.lock().unwrap();
-        Ok(lock_result
+        let guard = self.tickets_store.lock().unwrap();
+        Ok(guard
             .iter()
             .filter_map(|o| o.as_ref().map(|ticket| ticket.clone()))
             .collect())
+    }
+
+    async fn delete_ticket(&self, id: u64) -> Result<()> {
+        let mut guard = self.tickets_store.lock().unwrap();
+        let opt = guard.iter_mut().find(|o| {
+            o.as_ref().is_some_and(|t| t.id == id)
+        });
+        match opt {
+            Some(opt) => {
+                opt.take();
+                return Ok(());
+            }
+            None => {
+                return Err(Error::TicketDeleteFailIdNotFound { id });
+            }
+        }
     }
 }
